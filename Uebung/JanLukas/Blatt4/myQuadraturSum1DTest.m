ac6aa5578669d607d195be78fce66ec5a1b0e023
Jan Lukas Späh <janlukas.spaeh@tu-dortmund.de>
2018-05-20 14:56:52 +0200

    working version for 4.1 a)

--- Uebung/JanLukas/Blatt4/myQuadraturSum1DTest.m
+++ Uebung/JanLukas/Blatt4/myQuadraturSum1DTest.m
@@ -1,14 +1,14 @@
 1;
-
 function [c]=myQuadraturSum1D(f,w,p,a,b,N)
   sum = 0;
   temp = 0;
-  h = (b-a)/N;
+  h = (b-a)/N
   R = length(w);
   phi = zeros(N,R);
+  w = (b-a)/N * w
   for i = 1:N
     for j = 1:R
-      phi(i,j) = h/(R-1)*j - R*h/(R-1)+a+i*h;
+      phi(i,j) = a + (i-1)*h + p(j)*h;
     endfor
   endfor
   for i = 1:N
@@ -21,13 +21,11 @@ function [c]=myQuadraturSum1D(f,w,p,a,b,N)
   [c] = temp;
 endfunction
 
-f = @(x) exp(x);
-w = [1/8 3/8 3/8 1/8];
-p = [0 1/3 2/3 1];
+f = @(x) exp(x)+1;
+w = [1/4 1/2 1/4];
+p = [0 1/2 1];
 a = -1;
 b = 1;
-N = 2;
+N = 3;
 myQuadraturSum1D(f,w,p,a,b,N)
-% This only works for equidistant nodes. Works well, but p is not used and we still
-% have to implement 'dynamic', non-equidistant nodes!!
 
