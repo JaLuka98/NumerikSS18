1f0983fd55497eda5dbf2f27a8bd9ddc2f920234
Christopher Krause <christopher2.krause@tu-dortmund.de>
2018-06-09 19:03:16 +0200

    Numerik

--- Uebung/JanLukas/Blatt6/myBisectTest.m
+++ Uebung/JanLukas/Blatt6/myBisectTest.m
@@ -1,5 +1,5 @@
 f = @(x) cos(2.*x).^2-x.^2; % Thats the given function
 [x,e,v] = mybisect(f,0,0.75) % intial a is 0, initial b is 0.75
 semilogy(linspace(1,length(e),length(e)), e) % semilogy plot of the absolute error
-savefig('PA7.1.fig')
+save('PA7.1.fig')
 print("PA7.1.pdf");
