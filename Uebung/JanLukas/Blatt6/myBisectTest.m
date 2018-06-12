3157de852b99d9ff2efbd2873e48a4c9a5ac2569
Jan Lukas Späh <janlukas.spaeh@tu-dortmund.de>
2018-06-12 10:53:59 +0200

    added final version for assignment 6

--- Uebung/JanLukas/Blatt6/myBisectTest.m
+++ Uebung/JanLukas/Blatt6/myBisectTest.m
@@ -1,3 +1,30 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%  Numerische Mathematik fuer Physik und Ingenieurwissenschaften 2018     %%%
+%%   Programmierabgaben (Praktischer Teil des Uebungungsplattes)            %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%  Student 1: Jan Lukas, SpÃ¤h
+%%  Unimail-adress: janlukas.spaeh@tu-dortmund.de
+%%
+%%  Student 2: Christopher, Krause
+%%  Unimail-adress: christopher2.krause@tu-dortmund.de
+%%
+%%  Student 3: Maximilian, Krebs
+%%  Unimail-adress: maximilian.krebs@tu-dortmund.de
+%%
+%% Uebungszettel-Nr: Blatt 6
+%% Aufgabennummer:   6.1
+%% Program name:     myBisectTest.m
+%%
+%% Program(version): Octave 4.2.2
+%% OS:               x86_64-w64-mingw32
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Description of the program
+% This program tests the method "mybisect" implemented in mybisect.m to
+% numerically calculate a zero of the function f with f(x)=cos(2x)-x^2.
+% The starting interval is [0,0.75]. The Behaviour is as expected because
+% the length of the interval approximately follows 2^n.
+
 f = @(x) cos(2.*x).^2-x.^2; % Thats the given function
 [x,e,v] = mybisect(f,0,0.75) % intial a is 0, initial b is 0.75
 semilogy(linspace(1,length(e),length(e)), e) % semilogy plot of the absolute error
--- Uebung/JanLukas/Blatt6/mybisect.m
+++ Uebung/JanLukas/Blatt6/mybisect.m
@@ -1,6 +1,31 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%  Numerische Mathematik fuer Physik und Ingenieurwissenschaften 2018     %%%
+%%   Programmierabgaben (Praktischer Teil des Uebungungsplattes)            %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%  Student 1: Jan Lukas, SpÃ¤h
+%%  Unimail-adress: janlukas.spaeh@tu-dortmund.de
+%%
+%%  Student 2: Christopher, Krause
+%%  Unimail-adress: christopher2.krause@tu-dortmund.de
+%%
+%%  Student 3: Maximilian, Krebs
+%%  Unimail-adress: maximilian.krebs@tu-dortmund.de
+%%
+%% Uebungszettel-Nr: Blatt 6
+%% Aufgabennummer:   6.1
+%% Program name:     myBisect.m
+%%
+%% Program(version): Octave 4.2.2
+%% OS:               x86_64-w64-mingw32
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Description of the program
+% Implementation of the bisection method to find a root of f. Straightforward and not difficult,
+% the algorithm can be found everywhere...
+
 function [x,e,v]=mybisect(f,x00,x0)
   % first we rename the bounds of the interval
-  a = x00; 
+  a = x00;
   b = x0;
   n = 0; %number of iterations needed, starting at 1 inside the loop
   error = b - a; % the initial error is just the length of the interval
@@ -10,7 +35,7 @@ function [x,e,v]=mybisect(f,x00,x0)
     error = abs(b - x(n)); % Thats fine because biection is symmetric
     e(n) = error;
     v(n) = f(x(n)); % we want to give back the values of the function at the new bounds of the intervals as well
-    if f(a)*f(x(n)) < 0 % if the sign change occurs within [a_n,x_n] 
+    if f(a)*f(x(n)) < 0 % if the sign change occurs within [a_n,x_n]
       b = x(n); % change the interval to [a_n,x_n]
     elseif f(x(n))*f(b) < 0 % the same for [x_n, b_n]
       a = x(n);
