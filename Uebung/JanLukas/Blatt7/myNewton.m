a80fa26d5ccd13834cff3a7caf334269bb528d29
Jan Lukas Späh <janlukas.spaeh@tu-dortmund.de>
2018-06-15 21:56:38 +0200

    now the suggestion for assignment 7 is even prettier

--- Uebung/JanLukas/Blatt7/myNewton.m
+++ Uebung/JanLukas/Blatt7/myNewton.m
@@ -1,19 +1,43 @@
-function [x,e,v]=myNewton(f,df,x0)
-  n = 1;
-  x(1) = x0;
-  if f(x0) ~= 0
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%  Numerische Mathematik fuer Physik und Ingenieurwissenschaften 2018     %%%
+%%   Programmierabgaben (Praktischer Teil des Uebungungsplattes)            %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%  Student 1: Jan Lukas, SpÃ¤h
+%%  Unimail-adress: janlukas.spaeh@tu-dortmund.de
+%%
+%%  Student 2: Christopher, Krause
+%%  Unimail-adress: christopher2.krause@tu-dortmund.de
+%%
+%%  Student 3: Maximilian, Krebs
+%%  Unimail-adress: maximilian.krebs@tu-dortmund.de
+%%
+%% Uebungszettel-Nr: Blatt 7
+%% Aufgabennummer:   7.1
+%% Program name:     myNewton.m
+%%
+%% Program(version): Octave 4.2.2
+%% OS:               x86_64-w64-mingw32
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Description of the program
+% Implementation of newton-raphson to find a root of f. Straightforward and not difficult,
+% the algorithm can be found everywhere...
+function [x,e,v]=myNewton(f,df,x0) % f is a function handle and df is function handle of the derivative of it. we begin searching at x0
+  n = 1; % we store the number of iterations in n
+  x(1) = x0; % our first x is obviously the starting point
+  if f(x0) ~= 0 % Just in case we hit the root directly, then error is 1 and we never go into the loop at all.
     error = 1;
   else
     error = 0;
   endif
-  while error > 1e-12
-    v(n) = f(x(n));
-    if n >= 50
+  while error > 1e-12 % We want to be accurate to a degree of 12 decimal places
+    v(n) = f(x(n)); % always store the functions values in the v array
+    if n >= 50 % In case the method doesnt convergece when dont want to be stuck in an infinite loop
       break;
     endif
-    x(n+1) = x(n) - f(x(n))/df(x(n));
-    error =  abs(x(n+1) - x(n));
+    x(n+1) = x(n) - f(x(n))/df(x(n)); % Thats just the newton raphson method
+    error =  abs(x(n+1) - x(n)); % Clearly this is the error in estimating the root
     e(n) = error;
-    n = n + 1;
+    n = n + 1; % we have done one step
   endwhile
 endfunction
--- Uebung/JanLukas/Blatt7/myNewtonTest.m
+++ Uebung/JanLukas/Blatt7/myNewtonTest.m
@@ -1,11 +1,39 @@
-f = @(x) cos(2.*x).^2 - x.^2;
-df = @(x) -2 * (x + sin(4.*x));
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%  Numerische Mathematik fuer Physik und Ingenieurwissenschaften 2018     %%%
+%%   Programmierabgaben (Praktischer Teil des Uebungungsplattes)            %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%  Student 1: Jan Lukas, SpÃ¤h
+%%  Unimail-adress: janlukas.spaeh@tu-dortmund.de
+%%
+%%  Student 2: Christopher, Krause
+%%  Unimail-adress: christopher2.krause@tu-dortmund.de
+%%
+%%  Student 3: Maximilian, Krebs
+%%  Unimail-adress: maximilian.krebs@tu-dortmund.de
+%%
+%% Uebungszettel-Nr: Blatt 7
+%% Aufgabennummer:   7.1
+%% Program name:     myNewtonTest.m
+%%
+%% Program(version): Octave 4.2.2
+%% OS:               x86_64-w64-mingw32
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Description of the program
+% Testing and comparing the two methods. Quite clearly the error is behaving as expected.
+% The error of the bisection method is decreasing exponentially (semilog) while
+% the approximation of the root with newton-raphson doubles in correct digits with each iteration.
+
+f = @(x) cos(2.*x).^2 - x.^2; % That is the function we want to get a root of
+df = @(x) -2 * (x + sin(4.*x)); % df is f'(x)
 
 [xb,eb,vb] = mybisect(f,0,0.75)
 [xn,en,vn] = myNewton(f,df,0.75)
-semilogy(linspace(1,length(eb),length(eb)),eb)
+semilogy(linspace(1,length(eb),length(eb)),eb, "Bisection method")
 hold on
-semilogy(linspace(1,length(en),length(en)),en)
+semilogy(linspace(1,length(en),length(en)),en, "Newton-Raphson method")
 hold off
+legend (h, "location", "northeast");
+title ("Comparison of the convergence rate of Newton-Raphson method and bisection method.");
 save('plot.fig')
 print("plot.pdf");
